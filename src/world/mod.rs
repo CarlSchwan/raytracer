888a39ba757778ffeb3e15c3e2268e880e81f358
Carl Schwan <schwancarl@protonmail.com>
2018-12-18 17:30:02 +0100

    empty implementation

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -2,6 +2,7 @@ use crate::intersection::Intersection;
 use crate::ray::Ray;
 use crate::world::{light::Light, obj::Obj, plane::Plane, triangle::Triangle};
 use image::{GenericImage, ImageBuffer};
+use std::path::Path;
 
 pub mod light;
 pub mod obj;
@@ -13,7 +14,8 @@ pub trait Interceptable {
 }
 
 pub struct World {
-    //image: ImageBuffer,
+    width: usize,
+    height: usize,
     elements: Vec<Element>,
 }
 
@@ -22,4 +24,34 @@ pub enum Element {
     Triangle(Triangle),
     Plane(Plane),
     Obj(Obj),
+}
+
+impl World {
+    pub fn new(width: usize, height: usize) -> Self {
+        World { width, height, elements: Vec::new() }
+    }
+
+    pub fn add_sphere(&mut self, sphere: Sphere) -> Self {
+        // TODO
+        self
+    }
+
+    pub fn add_plane(&mut self, plane: Plane) -> Self {
+        // TODO
+        self
+    }
+
+    pub fn add_triangle(&mut self, triangle: Triangle) -> Self {
+        // TODO
+        self
+    }
+
+    pub fn add_obj(&mut self, obj_path: Path) -> Self {
+        // TODO
+        self
+    }
+
+    pub fn render(&self) /* -> ImgBuffer */ {
+
+    }
 }
