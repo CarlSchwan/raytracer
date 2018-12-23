9c132851f30983dea2d88fd9598343b8a8e43322
sasehash <s8sashal@stud.uni-saarland.de>
2018-12-24 00:33:13 +0100

    started to implement the plane

--- src/world/plane.rs
+++ src/world/plane.rs
@@ -1,1 +1,19 @@
-pub struct Plane;
+use crate::intersection::Intersection;
+use crate::ray::Ray;
+use crate::world::Interceptable;
+use image::Rgba;
+use na::{Vector3, Unit};
+
+pub struct Plane {
+    pub normal: Unit<Vector3<f64>>,
+    pub d: f64,
+    pub color: Rgba<u8>,
+    pub opacity: f64,
+    pub reflection: f64,
+}
+
+impl Interceptable for Plane {
+    fn intercept(&self, ray:&Ray) -> Option<(f64, Intersection)> {
+        unimplemented!();
+    }
+}
