8c1fa02874a8fd189321fab5bba58364d1a29910
Carl Schwan <schwancarl@protonmail.com>
2019-01-07 17:52:54 +0100

    fix bug light.position

--- src/world/light.rs
+++ src/world/light.rs
@@ -10,11 +10,11 @@ pub struct Light {
 impl Light {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         let color = Rgba::from_channels(1.0, 1.0, 1.0, 1.0);
-        let position = Vector3::new(x, y, z);
+        let pos = Vector3::new(x, y, z);
         let intensity = 1.0;
         Light {
             color,
-            position,
+            pos,
             intensity,
         }
     }
