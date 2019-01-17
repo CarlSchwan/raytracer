7709d56fb81c456a3298f94bf01e72aafb419eac
D. Freiermuth <d_freiermu14@cs.uni-kl.de>
2019-01-17 15:09:02 +0100

    Remove alpha from additive_shader
    #20

--- src/shader/additive_shader.rs
+++ src/shader/additive_shader.rs
@@ -6,15 +6,13 @@ use std::ops::Add;
 pub struct AdditiveShader {
     pub shader1: Box<Shader>,
     pub shader2: Box<Shader>,
-    pub alpha1: f64,
-    pub alpha2: f64,
 }
 
 impl Shader for AdditiveShader {
     fn get_appearance_for(&self, intersection_pos: Vector3<f64>, ray_dir: Vector3<f64>, surface_normal: Vector3<f64>, world: &World, surface_pos: Vector2<f64>, recursion_depth: u64) -> Vector3<f64> {
         let ap1 = self.shader1.get_appearance_for(intersection_pos, ray_dir, surface_normal, world, surface_pos, recursion_depth);
         let ap2 = self.shader2.get_appearance_for(intersection_pos, ray_dir, surface_normal, world, surface_pos, recursion_depth);
-        self.alpha1 * ap1 + self.alpha2 * ap2
+        ap1 + ap2
     }
 }
 // Add 2 Boxed Shader
@@ -22,7 +20,8 @@ impl Add for Box<Shader> {
     type Output = Box<Shader>;
 
     fn add(self, other: Box<Shader>) -> Box<Shader> {
-        Box::new(AdditiveShader { shader1:self, shader2:other, alpha1:1.0, alpha2:1.0 })
+        Box::new(AdditiveShader { shader1:self, shader2:other
+})
     }
 }
 
@@ -31,7 +30,7 @@ impl<T:Shader+'static> Add<T> for Box<Shader> {
     type Output = Box<Shader>;
 
     fn add(self, other: T) -> Box<Shader> {
-        Box::new(AdditiveShader { shader1: self, shader2: Box::new(other), alpha1: 1.0, alpha2: 1.0 })
+        Box::new(AdditiveShader { shader1: self, shader2: Box::new(other)})
     }
 }
 
