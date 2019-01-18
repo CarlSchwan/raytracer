b5a90c8f5d429d1e2a5aa695a958af2182da072e
D. Freiermuth <d_freiermu14@cs.uni-kl.de>
2019-01-18 13:20:02 +0100

    UV-Coords for plane
     #14

--- src/world/plane.rs
+++ src/world/plane.rs
@@ -5,34 +5,38 @@ use crate::shader::Shader;
 use na::{Unit, Vector3, Vector2};
 
 pub struct Plane {
-    pub normal: Unit<Vector3<f64>>, // Vektor, senkrecht zur Ebene
-    pub d: f64,                 // Distanz der Ebene zum Ursprung
+    pub a: Vector3<f64>,
+	pub b: Vector3<f64>,
+	pub c: Vector3<f64>,
     pub shader: Box<Shader>,
 }
 
 impl Interceptable for Plane {
     fn intercept(&self, ray: &Ray) -> Option<(f64, Intersection)> {
-        let convergence_rate = ray.dir.into_inner().dot(&self.normal.into_inner());
+		let edge_ab = (self.a - self.b).normalize();
+		let edge_ac = (self.a - self.c).normalize();
+		let	normal = edge_ab.cross(&edge_ac);
+        let convergence_rate = ray.dir.into_inner().dot(&normal);
         if convergence_rate == 0.0 {
             return None;
         }
 
-        let intersection_distance = (self.d - self.normal.into_inner().dot(&ray.start)) / convergence_rate;
+        let intersection_distance = normal.dot(&(self.a -ray.start)) / convergence_rate;
         if intersection_distance < 0.0 {
             return None;
         }
 
         let intersection_pos = intersection_distance * ray.dir.into_inner() + ray.start;
         let normal = if convergence_rate > 0.0 {
-                            - self.normal.into_inner()
+                            - normal
                         } else {
-                            self.normal.into_inner()
+                            normal
                         };
         let intersection = Intersection {
             pos: intersection_pos,
             normal_at_surface: normal,
             shader: &self.shader,
-            pos_on_surface: Vector2::new(0.0,0.0),//TODO
+            pos_on_surface: Vector2::new((self.a -intersection_pos).dot(&edge_ab),(self.a - intersection_pos).dot(&edge_ac)),
         };
         return Some((intersection_distance, intersection));
     }
