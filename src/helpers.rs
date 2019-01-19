be362f78702051f276029a43ef4d459c1f6ed06e
D. Freiermuth <d_freiermu14@cs.uni-kl.de>
2019-01-19 18:40:20 +0100

    Add polar2vector helper
    for #26

--- src/helpers.rs
+++ src/helpers.rs
@@ -10,3 +10,10 @@ use image::{Rgba, Rgb, Pixel};
 pub fn vector2color<T:na::Scalar + image::Primitive>(v : &Vector3<T>) -> Rgba<T> {
     Rgb::from_slice(v.as_slice()).to_rgba()
 }
+
+pub fn polar2vector(gamma: f64, phi: f64) -> Vector3<f64> {
+	let y = gamma.cos();
+	let x = gamma.sin() * phi.sin();
+	let z = gamma.sin() * phi.cos();
+	Vector3::new(x, y, z)
+}
