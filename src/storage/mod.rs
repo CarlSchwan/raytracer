f127c747eceb247f7229a18e9583ee056ea07e21
D. Freiermuth <d_freiermu14@cs.uni-kl.de>
2019-01-21 22:24:57 +0100

    Implement From<Box<Bounded>> for Box<Interceptable>

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,5 +1,7 @@
 use na::Vector3;
 use crate::world::Interceptable;
+use crate::intersection::Intersection;
+use crate::ray::Ray;
 
 pub mod Collector;
 mod BVStorage;
@@ -8,4 +10,20 @@ pub mod PrimitiveStorage;
 pub trait Bounded : Interceptable {
 	fn get_min(&self) -> Vector3<f64>;
 	fn get_max(&self) -> Vector3<f64>;
+}
+
+impl From<Box<Bounded>> for Box<Interceptable> {
+	fn from(element: Box<Bounded>) -> Self {
+		Box::new(InterceptFromBound{child: element})
+	}
+}
+
+struct InterceptFromBound {
+	child: Box<Bounded>,
+}
+
+impl Interceptable for InterceptFromBound {
+    fn intercept(&self, ray: &Ray) -> Option<(f64, Intersection)> {
+		self.child.intercept(ray)
+	}
 }
