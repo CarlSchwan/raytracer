3f166e06542b9500f900e599115cf75dd6968db1
D. Freiermuth <d_freiermu14@cs.uni-kl.de>
2019-01-21 23:38:26 +0100

    Adapt

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ mod error;
 mod storage;
 mod camera;
 
+use std::f64;
 use crate::world::sphere::*;
 use crate::world::plane::*;
 use crate::world::light::Light;
@@ -39,6 +40,7 @@ fn main() -> Result<(), Error> {
         file_parser.parse(argument)?;
     }
     let mut elements = file_parser.elements;
+	print!("File(s) read\n");
 
     // add some spheres
     let green_shader = get_phong(Vector3::new(0.0, 1.0, 0.0));
@@ -65,25 +67,26 @@ fn main() -> Result<(), Error> {
     }));
     elements.add(Box::new(Plane {
         normal: Unit::new_normalize(Vector3::new(0.0, 1.0, 0.0)),
-        d: 1.0,
+        d: -1.0,
         shader: blue_shader,
     }));
 
     // add light
     let mut lights = Vec::new();
-    lights.push(Light::new(0.0, -10.0, 6.0, Vector3::new(1.0, 0.5, 1.0)));
+    lights.push(Light::new(0.0, 10.0, 6.0, Vector3::new(1.0, 0.5, 1.0)));
 
 	let cam = EquilinearCamera {width: 300,
-					  height: 150,
-					  roll:-0.2, // down-up
-					  pitch: 0.2, //right-left
-					  yaw: 0.2, //rotation counterclockwise-clockwise
-					  pos: Vector3::new(0.0,0.0,0.0),
-					  vertical_viewangle:40.0,
+					  height: 300,
+					  roll:-0.3, // down-up
+					  pitch: 3.141, //right-left
+					  yaw: 0.0, //rotation counterclockwise-clockwise
+					  pos: Vector3::new(0.0,0.0,20.0),
+					  vertical_viewangle:70.0,
 					 };
 
     let w = world::World::new(elements.into_storage(), lights);
-    let image = cam.render(w);
+	print!("Storage prepared\n");
+    let image = cam.render(&w);
     image.save("./output.png")?;
     Ok(())
 }
