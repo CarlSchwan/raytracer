4d43397b72a528a9fff26eab6cb6aea48bcf0c35
sasehash <s8sashal@stud.uni-saarland.de>
2019-01-27 12:48:58 +0100

    compiles an runs now

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ use crate::camera::equilinear_camera::*;
 use wavefront_obj::obj::*;
 use std::env;
 use crate::obj::FileParser;
+use std::rc::Rc;
 use crate::error::Error;
 
 fn main() -> Result<(), Error> {
@@ -43,16 +44,36 @@ fn main() -> Result<(), Error> {
     let green_shader = get_phong(Vector3::new(0.0, 1.0, 0.0));
     let red_shader = get_phong(Vector3::new(1.0, 0.0, 0.0));
     let blue_shader = get_phong(Vector3::new(0.0, 0.0, 1.0));
+    let red_blue_shader = Box::new(chess_shader::ChessShader { shader1: red_shader, shader2: blue_shader, size:1.0});
+    let red_shader = get_phong(Vector3::new(1.0, 0.0, 0.0));
+    let blue_shader = get_phong(Vector3::new(0.0, 0.0, 1.0));
+    let red_blue_shader2 = Box::new(chess_shader::ChessShader { shader1: red_shader, shader2: blue_shader, size:1.0});
+    let red_shader = get_phong(Vector3::new(1.0, 0.0, 0.0));
+    let blue_shader = get_phong(Vector3::new(0.0, 0.0, 1.0));
 
+    elements.add(Box::new(Sphere {
+        center: Vector3::new(1.0, 1.0, 4.0),
+        radius: 1.0,
+        shader: red_blue_shader,
+        pitch: 0.0,
+        roll : 0.0,
+        yaw : 0.0,
+    }));
     elements.add(Box::new(Sphere {
         center: Vector3::new(0.0, 1.0, 6.0),
         radius: 1.0,
         shader: red_shader,
+        pitch: 0.0,
+        roll : 0.0,
+        yaw : 0.0,
     }));
     elements.add(Box::new(Sphere {
         center: Vector3::new(1.0, -1.0, 5.0),
         radius: 1.0,
         shader: green_shader,
+        pitch: 0.0,
+        roll : 0.0,
+        yaw : 0.0,
     }));
     elements.add(Box::new(Sphere {
         center: Vector3::new(2.0, 0.0, 9.0),
@@ -60,25 +81,29 @@ fn main() -> Result<(), Error> {
         shader: Box::new(MirrorShader{
             initial_step: 0.001,
         }),
+        pitch: 0.0,
+        roll : 0.0,
+        yaw : 0.0,
     }));
     elements.add(Box::new(Plane {
-        normal: Unit::new_normalize(Vector3::new(0.0, 1.0, 0.0)),
-        d: 1.0,
-        shader: blue_shader,
+        a: Vector3::new(0.0, 1.0, 0.0),
+        b: Vector3::new(1.0, 1.0, 0.0),
+        c: Vector3::new(0.0, 1.0, 1.0),
+        shader: red_blue_shader2,
     }));
 
     // add light
     let mut lights = Vec::new();
     lights.push(Light::new(0.0, -10.0, 6.0, Vector3::new(1.0, 0.5, 1.0)));
 
-	let cam = EquilinearCamera {width: 300,
-					  height: 150,
-					  roll:-0.2, // down-up
-					  pitch: 0.2, //right-left
-					  yaw: 0.2, //rotation counterclockwise-clockwise
-					  pos: Vector3::new(0.0,0.0,0.0),
-					  vertical_viewangle:40.0,
-					 };
+    let cam = EquilinearCamera {width: 300,
+                      height: 150,
+                      roll:-0.2, // down-up
+                      pitch: 0.2, //right-left
+                      yaw: 0.2, //rotation counterclockwise-clockwise
+                      pos: Vector3::new(0.0,0.0,0.0),
+                      vertical_viewangle:40.0,
+                     };
 
     let w = world::World::new(elements.into_storage(), lights);
     let image = cam.render(&w);
