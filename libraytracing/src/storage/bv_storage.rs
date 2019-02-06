423e56bc34e9d764aeed2700844baa658ff5f529
D. Freiermuth <d_freiermu14@cs.uni-kl.de>
2019-02-06 18:27:52 +0100

    Add Copyright header

--- libraytracing/src/storage/bv_storage.rs
+++ libraytracing/src/storage/bv_storage.rs
@@ -1,3 +1,19 @@
+/**
+ * Copyright © 2019
+ * Sami Shalayel <sami.shalayel@tutamail.com>,
+ * Carl Schwan <carl@carlschwan.eu>,
+ * Daniel Freiermuth <d_freiermu14@cs.uni-kl.de>
+ *
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the LICENSE file for more details.
+ * 
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See the LICENSE
+ * file for more details. **/
+
 use crate::intersection::Intersection;
 use crate::ray::Ray;
 use crate::storage::primitive_storage::PrimitiveStorage;
